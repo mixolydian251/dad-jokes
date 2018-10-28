9636854c236a2fd54e73cf6652b41c257636efc6
Mel DeJesus <meldejesus@mels-mbp.home>
2018-10-28 12:01:54 -0400

    on_click cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,38 @@
 $( document ).ready(function() {
-    $.get("scripts/jokes.json", function( data ) {
-        $("#question").text(data[0].Q)
-        $("#punchline").text(data[0].A)
-    })
+
+    $.get("scripts/jokes.json", function(data){
+        var joke = {
+            data: data
+        }
+        random(joke)
+    });
+        
+    function random(joke) {
+        var random = Math.floor(Math.random() * joke.data.length)
+        joke.question = joke.data[random].Q;
+        joke.answer = joke.data[random].A;
+        response(joke);
+    }
+
+    function response(joke) {       
+        $("#q").on("click", function() {
+            show_question(joke)
+        })
+
+        $("#a").on("click", function() {
+            show_answer(joke)  
+        }) 
+    }
+
+    function show_question(joke) {
+        $("#question").text(joke.question)
+        $("#punchline").text("")
+    }
+
+    function show_answer(joke) {
+        $("#punchline").text(joke.answer)
+        random(joke)
+    }    
 })
+
+ 
