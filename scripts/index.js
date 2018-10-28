24654b4f406e8ee92c990236fd07eb64a759ab28
mixolydian251 <tjhmusic11@gmail.com>
2018-10-28 16:18:19 -0400

    added random joke on click and card animations

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+// Helper functions
+function generateRandomJoke(jokes) {
+  var jokeId = Math.floor(Math.random() * jokes.length);
+  $("#question").text(jokes[jokeId].Q);
+  $("#punchline").text(jokes[jokeId].A);
+}
+
+function handleClick(jokes) {
+  var question = $("div.card__question");
+  var punchline = $("div.card__punchline");
+  $("section.joke-container").click(function() {
+    if (punchlineShown) generateRandomJoke(jokes);
+    question.toggleClass("card__question--show");
+    punchline.toggleClass("card__punchline--show");
+    punchlineShown = !punchlineShown;
+  });
+}
+
+// State
+var punchlineShown = false;
+
+// On mount
+$(document).ready(function() {
+  $.get("scripts/jokes.json", function(jokes) {
+    generateRandomJoke(jokes);
+    handleClick(jokes);
+  });
+});
