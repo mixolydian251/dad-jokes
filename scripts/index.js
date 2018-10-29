42d5b7008b12040bf849df0771e9fe6796c5308e
mixolydian251 <tjhmusic11@gmail.com>
2018-10-29 18:36:11 -0400

    HTML based menu, open/close functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,23 @@
 // State
 var punchlineShown = false;
+var menuOpen = false;
 var control = {
-    audio: "on",
-    color: "yellow"
-}
- 
+  audio: true,
+  color: "yellow"
+};
+
 // On mount
 $(document).ready(function() {
-    readJokes()
+  readJokes();
+  handleMenuClick();
 });
 
 function readJokes() {
-    $.get("data/formatted_jokes.json", function(jokes) {
-      generateRandomJoke(jokes);
-      handleClick(jokes);
-    })}
+  $.get("data/formatted_jokes.json", function(jokes) {
+    generateRandomJoke(jokes);
+    handleJokeClick(jokes);
+  });
+}
 
 // Helper functions
 function generateRandomJoke(jokes) {
@@ -24,84 +27,119 @@ function generateRandomJoke(jokes) {
   $("#joke-id").text(`#${jokeId}`);
 }
 
-function handleClick(jokes) {
+function handleJokeClick(jokes) {
   var question = $("div.card__question");
   var punchline = $("div.card__punchline");
-  // menu / joke click
-  $("menu").one("click", {jokes: jokes}, openMenu);   
+
+  // Next joke event
   $("section.joke-container").on("click", function(event) {
-    
     if (punchlineShown) generateRandomJoke(jokes);
     question.toggleClass("card__question--show");
     punchline.toggleClass("card__punchline--show");
     punchlineShown = !punchlineShown;
-    // audio controls
-    console.log(control.audio)
-    if (punchlineShown && (control.audio === "on")) {
-        var track = Math.ceil(Math.random() * 4);
-        console.log(track)
-        track = "../assets/audio/laugh_"+track+".wav";
-        setTimeout(function(){ 
-            new Audio(track).play() 
-        }, 1200);
+
+    // Audio controls
+    if (punchlineShown && control.audio) {
+      var track = Math.ceil(Math.random() * 4);
+      console.log(track);
+      track = "../assets/audio/laugh_" + track + ".wav";
+      setTimeout(function() {
+        new Audio(track).play();
+      }, 1200);
     }
   });
-}    
-
-function openMenu (jokes) {
-    var jokes = jokes.data.jokes;
-    $("section.joke-container").off("click");
-    $(".card").css("transform", "scale(1.5)");
-    $("#joke-id").hide();
-    // build checkbox
-    $("#audio").text("Audio Off")
-        .append($('<input />', { 
-            type: 'checkbox', 
-            class: 'cb', 
-            value: "off" 
-        }))
-    $(".cb").css({
-        "transform": "scale(2.5)", 
-        "margin-left":"10%", 
-        "margin-bottom":"15%"
-    })
-    // build dropdown
-    var quest = $("#question")
-    quest.text("Color Theme ")
-    var dropdown = ($('<select>', {id: 'color'}))
-        .append($('<option>').text("red"))
-        .append($('<option>').text("green"))
-        .append($('<option>').text("pink"))
-    dropdown.css({
-        "transform": "scale(1.25)"
-    })
-    quest.append(dropdown)
-    // build return
-    $("#extra").css({
-        "margin-top": "15%"
-    });
-    // read values on return
-    $("#extra").text("Return").one("click", function(event) {
-        //audio
-        control.audio = $('.cb:checked').val();
-        if (control.audio == undefined) {control.audio = "on"};
-        //color
-        control.color = $('#color').find(":selected").text();         
-        closeMenu(jokes)
-    })
 }
 
-function closeMenu(jokes) {
-    $("#extra").text("Return").off("click")
-    jokes == undefined ? jokes.data.jokes : jokes;
-    $("#joke-id").show();
-    $("#audio").text("");
-    $("#extra").text("")
-    $(".card").removeAttr("style");
-    $("body").css("background-color", control.color);
-    readJokes();
+function handleMenuClick() {
+  var menu = $(".menu");
+  $(".menu__button").on("click", function() {
+    animateMenuIcon();
+    if (!menuOpen) menu.removeClass("hidden");
+    else menu.addClass("hidden");
+    menuOpen = !menuOpen;
+  });
 }
- 
 
+function animateMenuIcon() {
+  var top = $("#menu__line--top");
+  var mid = $("#menu__line--middle");
+  var bottom = $("#menu__line--bottom");
+  if (menuOpen) {
+    top.removeClass("line__top--open");
+    mid.removeClass("line__middle--open");
+    bottom.removeClass("line__bottom--open");
+  } else {
+    top.addClass("line__top--open");
+    mid.addClass("line__middle--open");
+    bottom.addClass("line__bottom--open");
+  }
+}
 
-  
+// function openMenu(jokes) {
+//   var jokes = jokes.data.jokes;
+//   $("section.joke-container").off("click");
+//   $(".card").css("transform", "scale(1.5)");
+//   $("#joke-id").hide();
+
+//   // Build checkbox
+//   $("#audio")
+//     .text("Audio Off")
+//     .append(
+//       $("<input />", {
+//         type: "checkbox",
+//         class: "cb",
+//         value: "off"
+//       })
+//     );
+//   $(".cb").css({
+//     transform: "scale(2.5)",
+//     "margin-left": "10%",
+//     "margin-bottom": "15%"
+//   });
+
+//   // Build dropdown
+//   var quest = $("#question");
+//   quest.text("Color Theme ");
+//   var dropdown = $("<select>", { id: "color" })
+//     .append($("<option>").text("red"))
+//     .append($("<option>").text("green"))
+//     .append($("<option>").text("pink"));
+//   dropdown.css({
+//     transform: "scale(1.25)"
+//   });
+//   quest.append(dropdown);
+
+//   // Build return
+//   $("#extra").css({
+//     "margin-top": "15%"
+//   });
+
+//   // Read values on return
+//   $("#extra")
+//     .text("Return")
+//     .one("click", function(event) {
+//       //audio
+//       control.audio = $(".cb:checked").val();
+//       if (control.audio == undefined) {
+//         control.audio = "on";
+//       }
+//       // Color
+//       control.color = $("#color")
+//         .find(":selected")
+//         .text();
+//       closeMenu(jokes);
+//     });
+// }
+
+// function closeMenu(jokes) {
+//   $("#extra")
+//     .text("Return")
+//     .off("click");
+//   jokes == undefined ? jokes.data.jokes : jokes;
+//   $("#joke-id").show();
+//   $("#audio").text("");
+//   $("#extra").text("");
+//   $(".card").removeAttr("style");
+//   $("body").css("background-color", control.color);
+//   readJokes();
+// }
